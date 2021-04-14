198b2b385961e444ec8cc809c73c6c4ed2812be2
aidalaida <aidagilconde@gmail.com>
2021-04-14 10:25:55 +0200

    Lambdas

---
+++ funcionesLambda.py
@@ -0,0 +1,4 @@
+from funcionesPrimerNivel import sumaTodos
+
+print(sumaTodos(3, lambda x: x*2))
+print(sumaTodos(100, lambda x: x*3)) # o variable, triple = lambda x: x*3
--- funcionesPrimerNivel.py
+++ funcionesPrimerNivel.py
@@ -1,8 +1,14 @@
 def normal(x):
     return x
 
+#normal = lambda x: x #Cuando la funcion sea utilizable hay que usar def
+#Lambda solo en un momento, para algo muy simple.
+
 def cuadrado(y):
     return y * y
+
+def cubo(x):
+    return x**3
 
 def sumaTodos(limitTo, f):
     resultado = 0
@@ -11,5 +17,6 @@ def sumaTodos(limitTo, f):
     
     return resultado
 
-print(sumaTodos(100, normal))
-print(sumaTodos(3, cuadrado))
+if __name__ == '__main__':
+    print(sumaTodos(100, normal))
+    print(sumaTodos(3, cubo))
